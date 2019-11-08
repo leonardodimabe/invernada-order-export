260ad8804704436a5c2c8d09739797aad0a72772
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-08 09:00:39 -0300

    campos requeridos en embarque

--- export_order_dimabe/models/custom_shipment.py
+++ export_order_dimabe/models/custom_shipment.py
@@ -12,15 +12,20 @@ class CustomShipment(models.Model):
 
     shipping_company = fields.Many2one(
         comodel_name='custom.shipping.company',
-        string='Naviera'
+        string='Naviera',
+        required=True
     )
 
     ship = fields.Many2one(
         comodel_name='custom.ship',
-        string='Nave'
+        string='Nave',
+        required=True
     )
 
-    ship_number = fields.Char(string='Viaje')
+    ship_number = fields.Char(
+        string='Viaje',
+        required=True
+    )
 
     type_transport = fields.Selection(
         selection=[
@@ -28,28 +33,38 @@ class CustomShipment(models.Model):
             ('terrestre', 'Terrestre'),
             ('aereo', 'Aéreo')
         ],
-        string='Vía de Transporte'
+        string='Vía de Transporte',
+        required=True
     )
 
     departure_port = fields.Many2one(
         comodel_name='custom.port',
-        string='Puerto de salida'
+        string='Puerto de salida',
+        required=True
     )
 
     arrival_port = fields.Many2one(
         comodel_name='custom.port',
-        string='Puerto de llegada'
+        string='Puerto de llegada',
+        required=True
     )
 
-    required_loading_date = fields.Date(string='Fecha requerida de carga')
+    required_loading_date = fields.Date(
+        'Fecha requerida de carga',
+        required=True
+    )
 
     required_loading_week = fields.Integer(
         'Semana de Carga',
         compute='_compute_required_loading_week',
-        store=True
+        store=True,
+        required=True
     )
 
-    etd = fields.Date(string='ETD', nullable=True)
+    etd = fields.Date(
+        string='ETD',
+        nullable=True
+    )
 
     etd_month = fields.Integer(
         'Mes ETD',
@@ -63,11 +78,14 @@ class CustomShipment(models.Model):
         store=True
     )
 
-    eta = fields.Date(string='ETA', nullable=True)
+    eta = fields.Date(
+        string='ETA',
+        nullable=True
+    )
 
-    departure_date = fields.Datetime(string='Fecha de zarpe')
+    departure_date = fields.Datetime('Fecha de zarpe')
 
-    arrival_date = fields.Datetime(string='Fecha de arribo')
+    arrival_date = fields.Datetime('Fecha de arribo')
 
     @api.model
     @api.onchange('etd')
