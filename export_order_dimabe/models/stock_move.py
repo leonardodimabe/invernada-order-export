5bba3b29092e42e047f3f7d27c3c97f436b70d5e
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-19 14:33:15 -0300

    debug

---
+++ export_order_dimabe/models/stock_move.py
@@ -0,0 +1,15 @@
+from odoo import models, fields, api
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    reserved_amount = fields.Float(
+        'total',
+        compute='_compute_reserved_amount'
+    )
+
+    @api.model
+    def _compute_reserved_amount(self):
+        print('')
+
