3adcefc163d46b7336b391800dfc4fd213214aa4
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-19 09:49:44 -0300

    cambio orden models

--- export_order_dimabe/models/__init__.py
+++ export_order_dimabe/models/__init__.py
@@ -5,8 +5,8 @@ from . import custom_port
 from . import custom_ship
 from . import custom_shipment
 from . import custom_shipping_company
-from . import stock_picking
 from . import res_partner
 from . import custom_client_identifier
 from . import custom_contract
+from . import stock_picking
 
