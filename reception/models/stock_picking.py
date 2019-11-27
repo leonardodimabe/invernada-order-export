6fd3e5fbb951a9c553b14bf5fdf2aa34fa1ee0df
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-27 17:17:33 -0300

    debug

--- reception/models/stock_picking.py
+++ reception/models/stock_picking.py
@@ -17,6 +17,11 @@ class StockPicking(models.Model):
 
     def button_validate(self):
 
-        raise models.ValidationError(len(self.move_ids_without_package))
-
-        super(StockMoveLine, self).button_validate(self)
+        for stock_move in self.move_ids_without_package:
+            if stock_move.product_id.tacknig == 'serial':
+                counter = 1
+                for stock_move_line in stock_move.move_line_ids:
+                    tmp = '00{}'.format(counter)
+                    stock_move_line.lot_name = '{}-{}'.format(self.name, tmp[-3])
+                    counter += 1
+        super(StockPicking, self).button_validate(self)
