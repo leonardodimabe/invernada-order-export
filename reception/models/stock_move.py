4f4a6bad2f35aace89f227361eef664145e62837
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-28 09:05:38 -0300

    stock_move

---
+++ reception/models/stock_move.py
@@ -0,0 +1,29 @@
+from odoo import models, fields, api
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    has_serial_generated = fields.Boolean(
+        'tiene series generadas',
+        compute='_compute_has_serial_generated',
+        store=True
+    )
+
+    @api.model
+    def _compute_has_serial_generated(self):
+        if len(self.move_line.ids) > 0:
+            self.has_serial_generated = len(self.move_line_ids[0].lot_name) > 0
+        else:
+            self.has_serial_generated = False
+
+    @api.multi
+    def generate_serial(self):
+
+        for stock_move in self:
+            if stock_move.product_id.tracking == 'serial':
+                counter = 1
+                for stock_move_line in stock_move.move_line_ids:
+                    tmp = '00{}'.format(counter)
+                    stock_move_line.lot_name = '{}-{}'.format(stock_move.picking_id.name, tmp[-3:])
+                    counter += 1
--- reception/models/stock_picking.py
+++ reception/models/stock_picking.py
@@ -14,14 +14,3 @@ class StockPicking(models.Model):
     tare_weight = fields.Integer('Peso Tara')
 
     net_weight = fields.Integer('Kilos Netos')
-
-    def button_validate(self):
-
-        for stock_move in self.move_ids_without_package:
-            if stock_move.product_id.tracking == 'serial':
-                counter = 1
-                for stock_move_line in stock_move.move_line_ids:
-                    tmp = '00{}'.format(counter)
-                    stock_move_line.lot_name = '{}-{}'.format(self.name, tmp[-3:])
-                    counter += 1
-        super(StockPicking, self).button_validate()
