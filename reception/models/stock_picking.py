ec513c8fb754979c1453ff3471d14537071a3e7d
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-29 11:33:12 -0300

    debug

--- reception/models/stock_picking.py
+++ reception/models/stock_picking.py
@@ -21,3 +21,24 @@ class StockPicking(models.Model):
         'CSG',
         related='partner_id.sag_code'
     )
+
+    @api.multi
+    def action_confirm(self):
+        for stock_picking in self:
+            if stock_picking.is_mp_reception:
+                stock_picking.validate_mp_reception_fields()
+            res = super(StockPicking, self).action_confirm()
+            if stock_picking.is_mp_reception:
+                raise models.ValidationError(res)
+
+    @api.model
+    def validate_mp_reception_fields(self):
+        message = ''
+        if not self.guide_number:
+            message = 'debe agregar número de guía \n'
+        if not self.weight_guide:
+            message += 'debe agregar kilos guía \n'
+        if not self.gross_weight:
+            message += 'debe agregar kilos brutos \n'
+        if message:
+            raise models.ValidationError(message)
