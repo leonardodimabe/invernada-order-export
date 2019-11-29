d66bcecd6e82641e6c656fc89d6e5458084eeb17
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-11-29 15:19:50 -0300

    debug

--- reception/models/carrier.py
+++ reception/models/carrier.py
@@ -23,16 +23,16 @@ class Carrier(models.Model):
 
     @api.model
     def create(self, values_list):
-        if 'truck_patent' in values_list:
-            values_list['truck_patent'] = str.upper(values_list['truck_patent'])
-
+        self._prepare_data(values_list)
         return super(Carrier, self).create(values_list)
 
     @api.multi
     def write(self, vals):
-        if 'truck_patent' in vals:
-            vals['truck_patent'] = str.upper(vals['truck_patent'])
-        if 'cart_patent' in vals:
-            vals['cart_patent'] = str.upper(vals['cart_patent'])
-
+        self._prepare_data(vals)
         return super(Carrier, self).write(vals)
+
+    def _prepare_data(self, values_list):
+        if 'truck_patent' in values_list:
+            values_list['truck_patent'] = str.upper(values_list['truck_patent'])
+        if 'cart_patent' in values_list:
+            values_list['cart_patent'] = str.upper(values_list['cart_patent'])
