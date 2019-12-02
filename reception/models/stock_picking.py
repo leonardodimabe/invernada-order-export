0f40848153efb3d076854f2ba6cf37400ebd1438
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-02 11:54:11 -0300

    debug

--- reception/models/stock_picking.py
+++ reception/models/stock_picking.py
@@ -93,3 +93,9 @@ class StockPicking(models.Model):
             message += 'debe agregar MP y envases al listado de operaciones'
         if message:
             raise models.ValidationError(message)
+
+    @api.multi
+    def get_full_url(self):
+        self.ensure_one()
+        base_url = self.env["ir.config_parameter"].get_param("web.base.url")
+        return base_url
