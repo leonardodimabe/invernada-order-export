544246792b89d92eaa2eea64c77d6615a735fc4d
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-03 11:28:48 -0300

    debug

--- reception/models/stock_picking.py
+++ reception/models/stock_picking.py
@@ -61,7 +61,7 @@ class StockPicking(models.Model):
                 self.elapsed_time = (self.truck_out_date - self.date_done).total_seconds()
                 raise models.ValidationError('if {}'.format(self.elapsed_time))
             else:
-                self.elapsed_time = (datetime.today() - self.date_done).total_seconds()
+                self.elapsed_time = (datetime.now() - self.date_done).total_seconds()
                 raise models.ValidationError('else {}'.format(datetime.today()))
         else:
             self.elapsed_time = 0
