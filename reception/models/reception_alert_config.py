a035f2ccfadae496451a4eb67626f056c44fc028
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-03 15:51:29 -0300

    debug

--- reception/models/reception_alert_config.py
+++ reception/models/reception_alert_config.py
@@ -6,4 +6,15 @@ class ReceptionAlertConfig(models.Model):
 
     hr_alert = fields.Float('hr para alerta')
 
+    notify_elapsed_time_to = fields.Many2many(
+        'res.partner',
+        string='Notificar Retraso de Camión a'
+    )
+
     kg_diff_alert = fields.Float('diferencia de kg para alerta')
+
+    notify_diff_kg = fields.Many2many(
+        'res.partner',
+        string='Notificar Diferencia de kg a'
+    )
+
