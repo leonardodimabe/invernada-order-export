5942098a69322c48eca57b496a569b56db629017
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-04 12:24:33 -0300

    mail a manifest

--- reception/__manifest__.py
+++ reception/__manifest__.py
@@ -20,7 +20,11 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'stock'],
+    'depends': [
+        'base',
+        'stock',
+        'mail'
+    ],
 
     # always loaded
     'data': [
