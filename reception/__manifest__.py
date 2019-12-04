5b244c657d883b8b3032e9b07eee69073abcfa81
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-04 12:31:57 -0300

    debug

--- reception/__manifest__.py
+++ reception/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "reception",
+    'name': "Reception",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,8 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Dimabe ltda.",
+    'website': "http://www.dimabe.cl",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
