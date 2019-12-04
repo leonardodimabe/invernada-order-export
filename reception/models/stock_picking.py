aa0336908204edc3c5f809fe5404fc0053f57413
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-04 14:48:13 -0300

    debug

--- reception/models/stock_picking.py
+++ reception/models/stock_picking.py
@@ -145,9 +145,11 @@ class StockPicking(models.Model):
         # self.hr_alert_notification_count == 0 and
         if self.elapsed_time > alert_config.hr_alert:
 
-            # .with_context(destinies=alert_config.notify_elapsed_time_to.mapped('email'))
+            # d
             self.ensure_one()
-            template_id = self.env.ref('reception.truck_not_out_mail_template')
+            template_id = self.with_context(
+                destinies=alert_config.notify_elapsed_time_to.mapped('email')
+            ).env.ref('reception.truck_not_out_mail_template')
             self.message_post_with_template(template_id.id)
             self.hr_alert_notification_count += 1
 
