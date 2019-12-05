e08e8bc22c44594d43b4661ea44d1e48baff3050
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-05 17:24:39 -0300

    debug

--- reception/models/stock_move.py
+++ reception/models/stock_move.py
@@ -24,6 +24,7 @@ class StockMove(models.Model):
             ('type', 'in', ['product', 'consu']),
             ('categ_id', 'in', self.picking_type_id.warehouse_id.products_can_be_stored)
         ]
+        _logger.error('aaa {}'.format(self.picking_type_id))
         return domain
 
     @api.multi
