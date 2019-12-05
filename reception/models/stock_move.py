81da87f02a7e2dad43da05a058106eeb03f55dbe
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-05 15:27:35 -0300

    debug

--- reception/models/stock_move.py
+++ reception/models/stock_move.py
@@ -20,6 +20,7 @@ class StockMove(models.Model):
     )
 
     @api.model
+    @api.depends('picking_type_id')
     def _domain_filter(self):
         _logger.error('{} LALA'.format(self.id))
         domain = [
