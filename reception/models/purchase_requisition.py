4ecb4d3b87c90d5190eb718a4417a113727a3ad8
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-13 09:20:13 -0300

    debug

--- reception/models/purchase_requisition.py
+++ reception/models/purchase_requisition.py
@@ -6,7 +6,7 @@ class PurchaseRequisition(models.Model):
 
     @api.model
     def create(self, vals_list):
-        if 'ordering_date' not in vals_list:
+        if not vals_list['ordering_date']:
             vals_list['ordering_date'] = fields.datetime.now()
-        raise models.ValidationError(fields.datetime.now())
+        raise models.ValidationError( vals_list['ordering_date'] )
         return super(PurchaseRequisition, self).create(vals_list)
