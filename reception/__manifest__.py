5f2e6da6fcbc67ccdeaa0d5801830192c8139ed5
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-13 16:34:48 -0300

    debug

--- reception/__manifest__.py
+++ reception/__manifest__.py
@@ -16,7 +16,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'purchase_requisition',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -30,6 +30,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'security/groups.xml',
         'views/views.xml',
         'views/stock_picking.xml',
         'views/res_partner.xml',
