ba53f937330b12b71d0c87f26582fbd0df26e167
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-16 15:26:27 -0300

    debug

--- dimabe_purchase_process/models/purchase_requisition.py
+++ dimabe_purchase_process/models/purchase_requisition.py
@@ -18,9 +18,17 @@ class PurchaseRequisition(models.Model):
         self.message_post_with_template(template_id.id)
         return res
 
+    @api.multi
+    def action_open(self):
+        self.ensure_one()
+        res = super(PurchaseRequisition, self).action_open()
+        template_id = self.env.ref('dimabe_purchase_process.budget_ready_mail_template')
+        self.message_post_with_template(template_id.id)
+        return res
+
     @api.model
-    def get_email_to(self):
-        user_group = self.env.ref('dimabe_purchase_process.group_purchase_budget_user')
+    def get_email_to(self, ref_id):
+        user_group = self.env.ref(ref_id)
         email_list = [
             usr.partner_id.email for usr in user_group.users if usr.partner_id.email
         ]
