98599ab0136d5fef356cc222d3645493c8204932
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-16 10:53:45 -0300

    debug

--- dimabe_purchase_process/__manifest__.py
+++ dimabe_purchase_process/__manifest__.py
@@ -29,7 +29,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'security/groups.xml',
         'views/views.xml',
         'views/templates.xml',
