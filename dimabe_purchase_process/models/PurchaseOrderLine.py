9d03dc0811de8b4992616653f047b2dcb5f1c453
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-17 17:45:16 -0300

    debug

---
+++ dimabe_purchase_process/models/PurchaseOrderLine.py
@@ -0,0 +1,9 @@
+from odoo import fields, models, api
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    @api.onchange('product_qty', 'product_uom')
+    def _onchange_quantity(self):
+        return
--- dimabe_purchase_process/models/__init__.py
+++ dimabe_purchase_process/models/__init__.py
@@ -2,3 +2,4 @@
 
 from . import purchase_requisition
 from . import purchase_order
+from . import PurchaseOrderLine
