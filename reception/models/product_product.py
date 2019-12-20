1e54d882d5986afa7b56cf5c4ed9090b537d9723
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-20 09:08:56 -0300

    debug

--- reception/models/product_product.py
+++ reception/models/product_product.py
@@ -9,5 +9,5 @@ class ProductProduct(models.Model):
         variety = ''
         if self.is_product_variant:
             variety_variant = self.attribute_value_ids
-            raise models.ValidationError(variety_variant.name)
+            raise models.ValidationError(variety_variant.attribute_id.name)
         return variety
