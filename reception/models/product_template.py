7f8791db52c15c6feb56d63631290b01506aa277
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-20 08:48:52 -0300

    debug

--- reception/models/product_template.py
+++ reception/models/product_template.py
@@ -1,10 +1,10 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    variety_id = fields.Many2one(
-        'nut.variety',
-        string='Variedad'
-    )
+    @api.model
+    def get_variety(self):
+        variety_variant = self.product_variant_id.attribute_value_ids
+        raise models.ValidationError(variety_variant)
