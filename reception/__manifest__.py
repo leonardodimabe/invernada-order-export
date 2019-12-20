7db965029099328ca48098d5b5455766b366af87
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-20 08:51:03 -0300

    debug

--- reception/__manifest__.py
+++ reception/__manifest__.py
@@ -42,8 +42,7 @@
         'data/alert_config_data.xml',
         'data/reception_notification_mail_template.xml',
         'views/stock_warehouse.xml',
-        'views/product_category.xml',
-        'views/product_template.xml'
+        'views/product_category.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
