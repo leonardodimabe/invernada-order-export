26cacd501ba9a4c2c3906cedba9c85c9ca442750
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-26 09:45:56 -0300

    debug

--- reception/models/stock_move.py
+++ reception/models/stock_move.py
@@ -54,9 +54,7 @@ class StockMove(models.Model):
                         total_qty = stock_move.picking_id.get_canning_move().product_uom_qty
                         calculated_weight = stock_move_line.qty_done / total_qty
 
-                        raise models.ValidationError(total_qty)
-
-                        for i in range(total_qty):
+                        for i in range(int(total_qty)):
                             tmp = '00{}'.format(i)
                             serials.append({
                                 'calculated_weight': calculated_weight,
