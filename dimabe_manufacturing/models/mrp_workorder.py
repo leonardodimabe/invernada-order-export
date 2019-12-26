314cf356efe36dc416664c4679a98653ed8686ac
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-26 15:47:55 -0300

    debug

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -4,12 +4,12 @@ from odoo import fields, models, api
 class MrpWorkorder(models.Model):
     _inherit = 'mrp.workorder'
 
-    def action_next(self):
+    def do_finish(self):
 
-        # raise models.ValidationError(self.move_raw_ids)
+        raise models.ValidationError(self.move_raw_ids)
         # raise models.ValidationError(self.check_ids)
 
-        res = super(MrpWorkorder, self).action_next()
+        res = super(MrpWorkorder, self).do_finish()
 
         # raise models.ValidationError(self.move_raw_ids)
         # raise models.ValidationError(self.check_ids)
