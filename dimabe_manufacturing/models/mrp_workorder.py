c43a01a9009975d7d530fb8ccd8e29c2c05412e9
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-26 17:58:46 -0300

    debug

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -9,11 +9,9 @@ class MrpWorkorder(models.Model):
 
         res = super(MrpWorkorder, self).create(values_list)
 
-        raise models.ValidationError(res.product_id)
-
         final_lot = self.env['stock.production.lot'].create({
             'name': self.env['ir.sequence'].next_by_code('mrp.workorder'),
-            'product_id': self.product_id
+            'product_id': res.product_id
         })
 
         res.final_lot_id = final_lot.id
