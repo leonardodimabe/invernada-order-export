d8f5848fddc10107add10873c53e99774aa9b5ac
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-27 09:37:39 -0300

    debug

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -22,13 +22,14 @@ class MrpWorkorder(models.Model):
 
         for check in self.finished_product_check_ids.filtered(lambda a: a.component_is_byproduct):
             check.lot_id = self.final_lot_id.id
+            self._update_active_move_line()
 
         return res
 
     def action_next(self):
 
         # raise models.ValidationError(self.move_raw_ids)
-        raise models.ValidationError(self.finished_product_check_ids.mapped('component_is_byproduct'))
+        # raise models.ValidationError(self.finished_product_check_ids.mapped('component_is_byproduct'))
 
         res = super(MrpWorkorder, self).action_next()
 
