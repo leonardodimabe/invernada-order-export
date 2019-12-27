db412d14f13bc23b5bb258aa77feaa727cc16012
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-27 08:55:45 -0300

    debug

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -21,7 +21,7 @@ class MrpWorkorder(models.Model):
     def action_next(self):
 
         # raise models.ValidationError(self.move_raw_ids)
-        raise models.ValidationError(self.finished_product_check_ids)
+        raise models.ValidationError(self.finished_product_check_ids.mapped('component_is_byproduct'))
 
         res = super(MrpWorkorder, self).action_next()
 
