b858384661b4ed6ab1a09dd9edbcf7567e8e899d
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-12-27 08:52:22 -0300

    debug

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -20,8 +20,8 @@ class MrpWorkorder(models.Model):
 
     def action_next(self):
 
-        raise models.ValidationError(self.move_raw_ids)
-        # raise models.ValidationError(self.finished_product_check_ids)
+        # raise models.ValidationError(self.move_raw_ids)
+        raise models.ValidationError(self.finished_product_check_ids)
 
         res = super(MrpWorkorder, self).action_next()
 
