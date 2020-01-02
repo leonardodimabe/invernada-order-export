a46e1da3a4bf162e690430740bfb7d79b4486cd5
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-02 15:23:59 -0300

    debug

--- dimabe_manufacturing/models/mrp_production.py
+++ dimabe_manufacturing/models/mrp_production.py
@@ -8,6 +8,5 @@ class MrpProduction(models.Model):
     def calculate_done(self):
         for item in self:
             for line_id in item.finished_move_line_ids:
-                raise models.ValidationError(line_id.lot_id.total_serial)
                 line_id.qty_done = line_id.lot_id.total_serial
 
