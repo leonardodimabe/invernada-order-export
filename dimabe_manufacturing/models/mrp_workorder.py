24a5173596ba11d9c80874d8a2a8b39c13e332cb
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-03 17:53:48 -0300

    debug

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -46,9 +46,6 @@ class MrpWorkorder(models.Model):
         custom_serial = self.env['stock.production.lot.serial'].search([('serial_number', '=', barcode)])
         if custom_serial:
             barcode = custom_serial.stock_production_lot_id.name
-        if self.lot_id.name != barcode and self.lot_id:
-            self.action_next()
-            qty_done = 0
         super(MrpWorkorder, self).on_barcode_scanned(barcode)
         self.qty_done = qty_done + custom_serial.display_weight
 
