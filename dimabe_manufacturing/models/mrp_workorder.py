4eb1aaee3311efe5574a42ab059c49d982e8f454
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-03 12:51:26 -0300

    debug

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -32,12 +32,12 @@ class MrpWorkorder(models.Model):
 
                 if check.quality_state == 'none':
                     self.action_next()
-                raise models.ValidationError('{} {}'.format(check.lot_id, check.quality_state))
+
             else:
                 if not check.component_id.categ_id.is_canning:
                     check.qty_done = 0
                 self.action_skip()
-
+        raise models.ValidationError('{} {}')
         self.action_first_skipped_step()
 
         return res
