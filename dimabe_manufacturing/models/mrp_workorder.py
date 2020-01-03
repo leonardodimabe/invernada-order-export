925dfa904d2f9f92bd115bf9487c989b404837f3
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-03 12:40:22 -0300

    debug

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -37,7 +37,7 @@ class MrpWorkorder(models.Model):
                 self.action_skip()
 
         self.action_first_skipped_step()
-        raise models.ValidationError(self.finished_product_check_ids.mapped('quality_state'))
+        raise models.ValidationError(self.finished_product_check_ids.mapped('lot_id'))
         return res
 
     def action_next(self):
