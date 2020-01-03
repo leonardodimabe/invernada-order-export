b95b198c874dfcd8f1ef2952b94ce4546e12b022
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-03 12:19:15 -0300

    debug

--- reception/models/stock_move_line.py
+++ reception/models/stock_move_line.py
@@ -8,11 +8,11 @@ class StockMoveLine(models.Model):
     def create(self, values_list):
         res = super(StockMoveLine, self).create(values_list)
 
-        if res.move_id.picking_id and res.move_id.picking_type_code == 'incoming':
-            prefix = ''
-            if res.move_id.product_id.categ_id.is_canning:
-                prefix = 'ENV'
-            res.lot_name = '{}{}'.format(prefix, res.move_id.picking_id.name)
-            res.qty_done = res.product_uom_qty
+        #if res.move_id.picking_id and res.move_id.picking_type_code == 'incoming':
+        prefix = ''
+        if res.move_id.product_id.categ_id.is_canning:
+            prefix = 'ENV'
+        res.lot_name = '{}{}'.format(prefix, res.move_id.picking_id.name)
+        res.qty_done = res.product_uom_qty
 
         return res
