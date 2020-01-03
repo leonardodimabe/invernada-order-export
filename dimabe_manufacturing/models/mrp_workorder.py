ee95c7aa4d67aa1db85159e70cee41487c9fed24
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-03 12:28:08 -0300

    debug

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -21,6 +21,7 @@ class MrpWorkorder(models.Model):
         res = super(MrpWorkorder, self).open_tablet_view()
 
         for check in self.finished_product_check_ids:
+            raise models.ValidationError(self.finished_product_check_ids.mapped('display_name'))
             if check.component_is_byproduct:
                 if not check.lot_id:
                     lot_tmp = self.env['stock.production.lot'].create({
