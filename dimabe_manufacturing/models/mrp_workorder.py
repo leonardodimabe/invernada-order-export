e4abbb840418b448baba91bd689751423dc65c0f
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-03 14:08:39 -0300

    debug

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -32,7 +32,7 @@ class MrpWorkorder(models.Model):
                 if check.id == self.finished_product_check_ids[len(self.finished_product_check_ids) - 3].id:
                     raise models.ValidationError('{} {}'.format(
                         self.finished_product_check_ids.mapped('quality_state'),
-                        self.finished_product_check_ids.mapped('lot_id')
+                        self.finished_product_check_ids.mapped('lot_id').mapped('name')
                     ))
                 if check.quality_state == 'none':
                     self.action_next()
