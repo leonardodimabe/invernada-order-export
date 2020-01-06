2d9d18bb13d9540117cf2f91c247ac3f069df6e8
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-06 10:27:45 -0300

    debug

--- dimabe_manufacturing/__manifest__.py
+++ dimabe_manufacturing/__manifest__.py
@@ -32,6 +32,7 @@
         'views/stock_production_lot_serial.xml',
         'views/stock_production_lot.xml',
         'views/mrp_production.xml'
+        'reports/lot_serial_label_report.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
