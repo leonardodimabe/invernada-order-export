f1b3baffd29a924b450979e1a0fbc0b03b09de21
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-06 12:32:34 -0300

    debug

--- dimabe_manufacturing/models/__init__.py
+++ dimabe_manufacturing/models/__init__.py
@@ -4,3 +4,4 @@ from . import mrp_workorder
 from . import stock_production_lot
 from . import mrp_production
 from . import stock_production_lot_serial
+from . import stock_move_line
---
+++ dimabe_manufacturing/models/stock_move_line.py
@@ -0,0 +1,16 @@
+from odoo import fields, models, api
+
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+
+    count_stock_production_lot_serial = fields.Integer(
+        'Total Bultos',
+        compute='_compute_count_stock_production_lot_serial'
+    )
+
+    @api.multi
+    def _compute_count_stock_production_lot_serial(self):
+        for item in self:
+            if item.lot_id:
+                item.count_stock_production_lot_serial = sum(item.lot_id.stock_production_lot_serial_ids)
