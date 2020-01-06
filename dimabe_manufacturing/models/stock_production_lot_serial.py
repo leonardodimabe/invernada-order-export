47d986f4b08aced7d0adb3e98250adc826dcfcb9
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-06 10:38:22 -0300

    debug

--- dimabe_manufacturing/models/stock_production_lot_serial.py
+++ dimabe_manufacturing/models/stock_production_lot_serial.py
@@ -8,3 +8,9 @@ class StockProductionLotSerial(models.Model):
     def print_serial_label(self):
         return self.env.ref('dimabe_manufacturing.action_stock_production_lot_serial_label_report')\
             .report_action(self)
+
+    @api.multi
+    def get_full_url(self):
+        self.ensure_one()
+        base_url = self.env["ir.config_parameter"].sudo().get_param("web.base.url")
+        return base_url
