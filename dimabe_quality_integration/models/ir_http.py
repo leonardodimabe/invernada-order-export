45e30285c07d28c9a7456308276a8fcdcd9406fb
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-08 15:11:36 -0300

    debug

---
+++ dimabe_quality_integration/models/ir_http.py
@@ -0,0 +1,9 @@
+from odoo import exceptions, http, models
+from odoo.http import request
+
+
+class ItHttp(models.Model):
+    _inherit = 'ir.http'
+
+    def _auth_method_token(self):
+        raise exceptions.AccessDenied()
