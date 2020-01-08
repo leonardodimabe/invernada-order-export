e1f0258eac07ee73dc098e6e29a95097cfc636a4
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-08 11:11:30 -0300

    debug

--- dimabe_quality_integration/models/stock_production_lot.py
+++ dimabe_quality_integration/models/stock_production_lot.py
@@ -4,5 +4,5 @@ from odoo import fields, models, api
 class StockProductionLot(models.Model):
     _inherit = 'stock.production.lot'
 
-    quality_id = fields.Many2one('quality.analysis', 'Análisis de Calidad')
+    quality_analysis_id = fields.Many2one('quality.analysis', 'Análisis de Calidad')
 
