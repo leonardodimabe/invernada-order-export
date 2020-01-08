effedc4bf10f916b1eb2c70fb36206c4b69455d2
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-08 14:29:17 -0300

    debug

--- dimabe_quality_integration/controllers/quality_analysis_controller.py
+++ dimabe_quality_integration/controllers/quality_analysis_controller.py
@@ -1,5 +1,6 @@
 from odoo import http
 from odoo.http import request
+from xmlrpc import client
 
 
 class QualityAnalysis(http.Controller):
@@ -12,3 +13,23 @@ class QualityAnalysis(http.Controller):
             'pre_caliber',
             'caliber_ids'
         ])
+
+    @http.route('/api/login', type='json', auth='none', cors='*')
+    def login(self):
+        server_url = 'https://felipecarocadimabe-testerp-dev-792847.dev.odoo.com'
+        db_name = 'dev'
+        username = 'admin'
+        password = 'felipe'
+        common = client.ServerProxy('%s/xmlrpc/2/common' % server_url)
+        user_id = common.authenticate(db_name, username, password, {})
+        res = {}
+        if user_id:
+            res = {
+                'user_id': user_id
+            }
+        else:
+            res = {
+                'error': 'nop'
+            }
+        return res
+
