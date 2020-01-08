7565fe41d60b7edbed6514691247643fadd3fa2e
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-08 15:43:34 -0300

    debug

--- dimabe_quality_integration/models/ir_http.py
+++ dimabe_quality_integration/models/ir_http.py
@@ -6,6 +6,6 @@ class ItHttp(models.AbstractModel):
     _inherit = 'ir.http'
 
     def _auth_method_token():
-        # raise exceptions.AccessDenied()
+        raise exceptions.AccessDenied()
         exceptions._logger.error('aaaaaaaaaaaaa')
         print('')
