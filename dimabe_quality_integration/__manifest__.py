ad3bf538111ed0aaf77eb2a4683e7f985eb4ae19
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-08 10:46:10 -0300

    debug

--- dimabe_quality_integration/__manifest__.py
+++ dimabe_quality_integration/__manifest__.py
@@ -27,7 +27,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
+        'views/stock_picking.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
