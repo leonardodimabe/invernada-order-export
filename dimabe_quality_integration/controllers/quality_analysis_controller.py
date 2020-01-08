8eb964f953a5c21df1fed44a74ec8e9317d989ae
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-08 12:17:07 -0300

    debug

--- dimabe_quality_integration/controllers/quality_analysis_controller.py
+++ dimabe_quality_integration/controllers/quality_analysis_controller.py
@@ -4,7 +4,7 @@ from odoo.http import request
 
 class QualityAnalysis(http.Controller):
 
-    @http.route('/quality_analysis', type='http', auth='user', cors='*')
+    @http.route('/quality_analysis', type='http', auth='none', cors='*')
     def quality_analysis_list(self):
         res = request.env['quality.analysis'].sudo().search([])
         return {
