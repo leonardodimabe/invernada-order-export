ae3a95ebf51d594a8008917ebfe2df2cedfa579d
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-08 10:55:55 -0300

    debug

--- dimabe_quality_integration/models/stock_picking.py
+++ dimabe_quality_integration/models/stock_picking.py
@@ -5,5 +5,5 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     stock_move_line_lot_ids = fields.One2many(
-        related='move_ids_without_package.move_line_ids.lot_id'
+        related='move_ids_without_package.move_line_ids'
     )
