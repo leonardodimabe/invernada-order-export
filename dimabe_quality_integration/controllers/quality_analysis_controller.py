955ffd4233b432c8ce64ea26daa62be60e5ad235
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-08 12:11:20 -0300

    debug

--- dimabe_quality_integration/controllers/quality_analysis_controller.py
+++ dimabe_quality_integration/controllers/quality_analysis_controller.py
@@ -7,6 +7,9 @@ class QualityAnalysis(http.Controller):
     @http.route('/quality_analysis', type='json', auth='user', cors='*')
     def quality_analysis_list(self):
         res = request.env['quality.analysis'].sudo().search([])
+        return {
+            'lala': 'lala'
+        }
         return res.read([
             'pre_caliber',
             'caliber_ids'
